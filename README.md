f9bef563198d1e39002200867daf5dc6ac244ef3
ljw2487 <ljw248769961@qq.com>
2021-10-23 18:55:28 +0800

    update 21/10/23

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 # Web_Project
 一个web在线商城的项目
 
-> something...something...
+> 手办商城，用于练习基础技术
+
+#### 使用插件
+- express
+  - npm install express
+- mysql
+  - npm install mysql
 
 #### 项目内容
 
 - html
+  - index.html (homepage)
 - css
+  - stylesheet.css (scss)
 - js
+  - app.js (run server)
+  - js/basic.js (testingfile)
 - Node.js
 - sql
 
--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 const express = require("express")
+const querystring = require('querystring')
+const mysql = require('mysql')
+// const pool = mysql.createPool({
+//   host:'',
+//   port:'',
+//   user:'',
+//   password:'',
+//   databases:'',
+// })
 const app = express()
-app.use(express.static("static"))
+// ↓ - 设置本地服务器端口
+app.listen(8099, () => {
+  console.log('The server has been started on 8099 o(*￣▽￣*)ブ');
+})
+// ↓ - 将请求数据解析为对象
+app.use(express.urlencoded({ extended:false }))
+// ↓ - 设置静态资源目录
+app.use(express.static( "./static" ))
+// ↓ - 设置主页地址
+app.get( '/', function(req,res){
+  res.sendFile(__dirname + "/static/html/index.html")
+})
